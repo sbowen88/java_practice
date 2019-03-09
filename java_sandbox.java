18ca5d89a54f5f2cecab97044c60ee9dc94ba7e4
sbowen88 <sbowey@gmail.com>
2019-03-09 13:41:37 -0800

    another simple class

--- java_sandbox.java
+++ java_sandbox.java
@@ -5,3 +5,11 @@ public class HelloYou {
       
     }
   }
+
+  // Another simple class
+  public class Review {
+    public static void main(String[] args) {
+      // The main method executes the tasks of the class
+      System.out.println("My first Java program from scratch!");
+    }
+  }
