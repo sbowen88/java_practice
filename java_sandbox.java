f8bcc0df6644f09e71c6ae5a8bab63373022c90c
sbowen88 <sbowey@gmail.com>
2019-03-09 13:53:14 -0800

    simple variables

--- java_sandbox.java
+++ java_sandbox.java
@@ -13,3 +13,13 @@ public class HelloYou {
       System.out.println("My first Java program from scratch!");
     }
   }
+
+  //simple variables
+  public class Creator {
+	public static void main(String[] args) {
+    String name = "James Gosling";
+    int yearCreated = 1995;
+    System.out.println(name);
+    System.out.println(yearCreated);
+	}
+}
